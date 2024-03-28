75fee046f1b81a8b9e96fc9b4878f8204aa9fa19
Iryna Hrubiy <irynahrubiy@gmail.com>
2024-03-27 22:53:45 -0400

    window

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,4 +2,31 @@ import sys
 
 import pygame
 
-print("Hello World")
+from settings import Settings
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+
+            pygame.display.flip() #make the most recently drawn screen visible
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    #game instance and running the game
+    ai = AlienInvasion()
+    ai.run_game()
